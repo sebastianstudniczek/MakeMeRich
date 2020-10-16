fed8ce618d8853ab6e9cb5afef3d9a6375637541
Sebastian <sebastian.studniczek@outlook.com>
2020-10-16 20:13:53 +0200

    Create test for creating the financial account

---
+++ Application.IntegrationTests/FinancialAccounts/Commands/CreateFinancialAccountTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace MakeMeRich.Application.IntegrationTests.FinancialAccounts.Commands
+{
+    public class CreateFinancialAccountTests
+    {
+        [Fact]
+        public void ShouldCreateFinancialAccount()
+        {
+            // Arrange
+        }
+    }
+}
