19c27350190bf3cc1d71b4679c01fec354006b61
Sebastian <sebastian.studniczek@outlook.com>
2020-10-26 08:41:02 +0100

    Create mapping profile class and interface for mapping types

--- Source/Application/FinancialAccounts/Queries/FinancialAccountDto.cs
+++ Source/Application/FinancialAccounts/Queries/FinancialAccountDto.cs
@@ -1,6 +1,14 @@
-﻿namespace MakeMeRich.Application.FinancialAccounts.Queries
+﻿using System.Collections.Generic;
+
+using MakeMeRich.Application.Common.Mappings;
+using MakeMeRich.Domain.Entities;
+
+namespace MakeMeRich.Application.FinancialAccounts.Queries
 {
-    public class FinancialAccountDto
+    public class FinancialAccountDto : IMapFrom<FinancialAccount>
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IList<FinancialTransaction> Transactions { get; set; } = new List<FinancialTransaction>();
     }
 }
