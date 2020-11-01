740f7977b2c03816948ff2b2f415c1d28eeedb29
Sebastian <sebastian.studniczek@outlook.com>
2020-11-01 19:53:17 +0100

    Create base unit test for handler

---
+++ Tests/Application.UnitTests/FinancialTransactions/InternalTransactions/Commands/CreateInternalTransactionCommandHandlerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeMeRich.Application.UnitTests.FinancialTransactions.InternalTransactions.Commands
+{
+    class CreateInternalTransactionCommandHandlerTests
+    {
+    }
+}
