c1ac09f76e42182f2b3367afa1eb28050094fae6
Sebastian <sebastian.studniczek@outlook.com>
2020-11-02 19:25:55 +0100

    Create command test base class

---
+++ Tests/Application.UnitTests/Common/CommandTestBase.cs
@@ -0,0 +1,16 @@
+﻿using MakeMeRich.Application.UnitTests.Helper;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MakeMeRich.Application.UnitTests.Common
+{
+    public class CommandTestBase
+    {
+        public CommandTestBase()
+        {
+            DbContextOptions = ApplicationDbContextOptionsFactory.Create();
+        }
+
+        public DbContextOptions DbContextOptions { get;}
+    }
+}
