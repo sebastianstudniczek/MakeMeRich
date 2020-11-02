acd875f17f4f50502644d48e121a8a5da3a94108
Sebastian <sebastian.studniczek@outlook.com>
2020-11-02 19:27:12 +0100

    Fix seeding method in data seeder

--- Tests/Application.UnitTests/Helper/DataSeeder.cs
+++ Tests/Application.UnitTests/Helper/DataSeeder.cs
@@ -9,21 +9,20 @@ namespace MakeMeRich.Application.UnitTests.Helper
 {
     internal static class DataSeeder
     {
-        internal static void Seed(DbContextOptions<ApplicationDbContext> options)
+        internal static void SeedSampleData(DbContextOptions<ApplicationDbContext> options)
         {
-            using (var context = new ApplicationDbContext(options))
-            {
-                context.Database.EnsureDeleted();
+            var context = new ApplicationDbContext(options);
 
-                var financialAccounts = new List<FinancialAccount>
-                {
-                    new FinancialAccount {Id = 1, Title = "BNP", CurrentBalance = 100},
-                    new FinancialAccount {Id = 2, Title = "Getin", CurrentBalance = 200},
-                    new FinancialAccount {Id = 3, Title = "PKO", CurrentBalance = 300}
-                };
-                context.FinancialAccounts.AddRange(financialAccounts);
-                context.SaveChanges();
-            }
+            context.Database.EnsureCreated();
+
+            var financialAccounts = new List<FinancialAccount>
+            {
+                new FinancialAccount {Id = 1, Title = "BNP", CurrentBalance = 100},
+                new FinancialAccount {Id = 2, Title = "Getin", CurrentBalance = 200},
+                new FinancialAccount {Id = 3, Title = "PKO", CurrentBalance = 300}
+            };
+            context.FinancialAccounts.AddRange(financialAccounts);
+            context.SaveChanges();
         }
     }
 }
