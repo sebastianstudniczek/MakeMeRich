054acec76a1eb925c4bae45b6cc79fde5f48975f
Sebastian <sebastian.studniczek@outlook.com>
2020-11-02 15:58:26 +0100

    Create command handler test base

---
+++ Tests/Application.UnitTests/FinancialCategories/Commands/CreateFinancialCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using MakeMeRich.Domain.Entities;
+using MakeMeRich.Infrastructure.Persistance;
+
+using Microsoft.EntityFrameworkCore;
+
+using Xunit;
+
+namespace MakeMeRich.Application.UnitTests.FinancialCategories.Commands
+{
+    public class CreateFinancialCommandHandlerTests
+    {
+        [Fact]
+        public async Task ShouldCreateFinancialCategory()
+        {
+            int id;
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: nameof(ShouldCreateFinancialCategory))
+                .Options;
+
+            var command = new CreateFinancialCategoryCommand
+            {
+                Name = "House"
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var commandHandler =
+                    new CreateFinancialCategoryCommandHandler(context);
+
+                id = await commandHandler.Handle(command, CancellationToken.None);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var financialCategory = await context.FindAsync<FinancialCategory>(id);
+
+                financialCategory.Should().NotBeNull();
+                financialCategory.Name.Should().Be(command.Name);
+            }
+        }
+    }
+}
