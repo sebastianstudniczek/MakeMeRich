b53b598d618c56c1af56c015ca01eeb4d8b9fa0d
Sebastian <sebastian.studniczek@outlook.com>
2020-11-02 19:40:12 +0100

    Upgrade db context options factory to generic

--- Tests/Application.UnitTests/Helper/ApplicationDbContextOptionsFactory.cs
+++ Tests/Application.UnitTests/Helper/ApplicationDbContextOptionsFactory.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 
-using MakeMeRich.Infrastructure.Persistance;
+using MakeMeRich.Application.Common.Interfaces;
 
 using Microsoft.EntityFrameworkCore;
 
 namespace MakeMeRich.Application.UnitTests.Helper
 {
-    public static class ApplicationDbContextOptionsFactory
+    public static class ApplicationDbContextOptionsFactory<TContext> where TContext : DbContext, IApplicationDbContext
     {
-        public static DbContextOptions Create()
+        public static DbContextOptions<TContext> Create()
         {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            var options = new DbContextOptionsBuilder<TContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
                 .Options;
 
