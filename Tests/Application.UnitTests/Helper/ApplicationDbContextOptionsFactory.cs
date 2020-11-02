9119d44b1a62dc3d66333a4a57e8c111d305d684
Sebastian <sebastian.studniczek@outlook.com>
2020-11-02 19:26:31 +0100

    Create factory for db context options

---
+++ Tests/Application.UnitTests/Helper/ApplicationDbContextOptionsFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using MakeMeRich.Infrastructure.Persistance;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MakeMeRich.Application.UnitTests.Helper
+{
+    public static class ApplicationDbContextOptionsFactory
+    {
+        public static DbContextOptions Create()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return options;
+        }
+    }
+}
