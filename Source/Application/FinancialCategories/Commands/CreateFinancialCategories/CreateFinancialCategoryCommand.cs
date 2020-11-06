875f20f56b78a589fb8f0aeb4cac0825f4dd83ec
Sebastian <sebastian.studniczek@outlook.com>
2020-11-06 12:07:46 +0100

    Remove redundant command folder

--- Source/Application/FinancialCategories/Commands/CreateFinancialCategories/CreateFinancialCategoryCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace MakeMeRich.Application.FinancialCategories.Commands.CreateFinancialCategories
-{
-    public class CreateFinancialCategoryCommand : IRequest<int>
-    {
-        public string Name { get; set; }
-    }
-}
