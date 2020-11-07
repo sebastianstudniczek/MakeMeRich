8f5628d4f995686a39cdcc046fbdd03f238ad73a
Sebastian <sebastian.studniczek@outlook.com>
2020-11-07 15:20:31 +0100

    Create Infrastructure DI class

---
+++ Source/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using MakeMeRich.Application.Common.Interfaces;
+using MakeMeRich.Infrastructure.Persistance;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakeMeRich.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("MakeMeRichDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        options => options.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
