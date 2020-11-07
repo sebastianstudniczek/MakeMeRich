91da059682ee5989f4ec651540a34a27fa6e8db0
Sebastian <sebastian.studniczek@outlook.com>
2020-11-07 15:14:59 +0100

    Create internal transaction configuration

---
+++ Source/Infrastructure/Persistance/Configurations/FinancialTransactions/InternalTransactionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using MakeMeRich.Domain.Entities.FinancialTransactions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MakeMeRich.Infrastructure.Persistance.Configurations.FinancialTransactions
+{
+    public class InternalTransactionConfiguration : IEntityTypeConfiguration<InternalTransaction>
+    {
+        public void Configure(EntityTypeBuilder<InternalTransaction> builder)
+        {
+            builder.Property(property => property.TotalAmount)
+                .HasColumnType("decimal(10,2)")
+                .IsRequired();
+
+            builder.Property(property => property.DueDate)
+                .HasColumnType("date")
+                .IsRequired();
+
+            builder.Property(property => property.Description)
+                .HasMaxLength(150);
+        }
+    }
+}
