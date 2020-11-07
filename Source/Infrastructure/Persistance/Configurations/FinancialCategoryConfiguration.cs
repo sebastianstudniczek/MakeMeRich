8baa41777ac151ee0b00b45b3cb6f60bd1ab0a7b
Sebastian <sebastian.studniczek@outlook.com>
2020-11-07 13:24:29 +0100

    Add financial category configuration

---
+++ Source/Infrastructure/Persistance/Configurations/FinancialCategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using MakeMeRich.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MakeMeRich.Infrastructure.Persistance.Configurations
+{
+    public class FinancialCategoryConfiguration : IEntityTypeConfiguration<FinancialCategory>
+    {
+        public void Configure(EntityTypeBuilder<FinancialCategory> builder)
+        {
+            builder.Property(property => property.Name)
+                .HasMaxLength(80)
+                .IsUnicode()
+                .IsRequired();
+        }
+    }
+}
