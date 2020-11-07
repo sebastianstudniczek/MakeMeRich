a789fc059af369e4b8ee65a1e994b9500701b7f6
Sebastian <sebastian.studniczek@outlook.com>
2020-11-07 13:31:47 +0100

    Create TransactionCategory configuration

---
+++ Source/Infrastructure/Persistance/Configurations/TransactionCategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using MakeMeRich.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MakeMeRich.Infrastructure.Persistance.Configurations
+{
+    public class TransactionCategoryConfiguration : IEntityTypeConfiguration<TransactionCategory>
+    {
+        public void Configure(EntityTypeBuilder<TransactionCategory> builder)
+        {
+            builder.HasKey(
+                key => new
+                {
+                    key.FinancialTransactionId,
+                    key.FinancialCategoryId
+                });
+
+            builder.Property(property => property.Amount)
+                .HasColumnType("decimal(10,2)")
+                .IsRequired();
+
+            builder.Property(property => property.Description)
+                .HasMaxLength(150);
+        }
+    }
+}
