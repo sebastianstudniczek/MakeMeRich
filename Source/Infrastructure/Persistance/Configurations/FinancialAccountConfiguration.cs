38f8b18587350b7a3ccc10b4e9a5aadebad9063b
Sebastian <sebastian.studniczek@outlook.com>
2020-11-07 13:21:35 +0100

    Add financial account configuration

---
+++ Source/Infrastructure/Persistance/Configurations/FinancialAccountConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using MakeMeRich.Domain.Entities;
+using MakeMeRich.Domain.Enums;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MakeMeRich.Infrastructure.Persistance.Configurations
+{
+    public class FinancialAccountConfiguration : IEntityTypeConfiguration<FinancialAccount>
+    {
+        public void Configure(EntityTypeBuilder<FinancialAccount> builder)
+        {
+            builder.Property(property => property.Title)
+                .HasMaxLength(150)
+                .IsUnicode()
+                .IsRequired();
+
+            builder.Property(property => property.CurrentBalance)
+                .HasColumnType("decimal(10,2)")
+                .IsRequired();
+
+            builder.Property(prop => prop.Type)
+                .HasConversion(
+                    value => value.ToString(),
+                    value => (FinancialAccountType)Enum.Parse(typeof(FinancialAccountType), value));
+        }
+    }
+}
