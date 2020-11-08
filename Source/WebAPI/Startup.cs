3ea5c935067070fceb12233357ff8e06069870c6
Sebastian <sebastian.studniczek@outlook.com>
2020-11-08 20:06:03 +0100

    Register Infrastructure in API startup

--- Source/WebAPI/Startup.cs
+++ Source/WebAPI/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using MakeMeRich.Infrastructure;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +29,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddInfrastructure(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
