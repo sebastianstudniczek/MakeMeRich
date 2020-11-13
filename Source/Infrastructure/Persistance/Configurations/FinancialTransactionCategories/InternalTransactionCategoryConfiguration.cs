3dba55d68e8d4c6c09b1eb637b837bbb79556905
Sebastian <sebastian.studniczek@outlook.com>
2020-11-13 15:45:39 +0100

    Add FKs to internal transaction category config

--- Source/Infrastructure/Persistance/Configurations/FinancialTransactionCategories/InternalTransactionCategoryConfiguration.cs
+++ Source/Infrastructure/Persistance/Configurations/FinancialTransactionCategories/InternalTransactionCategoryConfiguration.cs
@@ -8,12 +8,15 @@ public class InternalTransactionCategoryConfiguration : IEntityTypeConfiguration
 {
     public void Configure(EntityTypeBuilder<InternalTransactionCategory> builder)
     {
-        builder.HasKey(
-                 key => new
-                 {
-                     key.InternalTransactionId,
-                     key.FinancialCategoryId
-                 });
+        builder
+            .HasOne(transactionCategory => transactionCategory.InternalTransaction)
+            .WithMany(category => category.TransactionCategories)
+            .HasForeignKey(transactionCategory => transactionCategory.InternalTransactionId);
+
+        builder
+            .HasOne(transactionCategory => transactionCategory.FinancialCategory)
+            .WithMany(category => category.InternalTransactionCategories)
+            .HasForeignKey(transactionCategory => transactionCategory.FinancialCategoryId);
 
         builder.Property(property => property.Amount)
             .HasColumnType("decimal(10,2)")
@@ -21,6 +24,13 @@ public class InternalTransactionCategoryConfiguration : IEntityTypeConfiguration
 
         builder.Property(property => property.Description)
             .HasMaxLength(150);
+
+        builder.HasKey(
+                 key => new
+                 {
+                     key.InternalTransactionId,
+                     key.FinancialCategoryId
+                 });
     }
 }
 
