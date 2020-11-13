0ca965e48e0d4e0c51dea9ba98b2f2b8897ee7e3
Sebastian <sebastian.studniczek@outlook.com>
2020-11-13 15:27:34 +0100

    Refactor financial transaction category class
    Fix naming of the financial category navigation property and
    removed inheritance from base entity.

--- Source/Domain/Entities/FinancialTransactionCategories/FinancialTransactionCategory.cs
+++ Source/Domain/Entities/FinancialTransactionCategories/FinancialTransactionCategory.cs
@@ -1,10 +1,10 @@
 ﻿namespace MakeMeRich.Domain.Entities.FinancialTransactionCategories
 {
-    public abstract class FinancialTransactionCategory : BaseEntity
+    public abstract class FinancialTransactionCategory
     {
         public double Amount { get; set; }
         public string Description { get; set; }
         public int FinancialCategoryId { get; set; }
-        public FinancialCategory Category { get; set; }
+        public FinancialCategory FinancialCategory { get; set; }
     }
 }
