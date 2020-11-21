88ca324e4842956afdafed0cef71b13ee63ecd6d
Sebastian <sebastian.studniczek@outlook.com>
2020-11-21 21:23:24 +0100

    Correct financial account create unit test

--- Application.IntegrationTests/FinancialAccounts/Commands/CreateFinancialAccountCommandHandlerTests.cs
+++ Application.IntegrationTests/FinancialAccounts/Commands/CreateFinancialAccountCommandHandlerTests.cs
@@ -1,18 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 using FluentAssertions;
 
-using MakeMeRich.Application.Common.Interfaces;
 using MakeMeRich.Application.FinancialAccounts.Commands.CreateFinancialAccount;
 using MakeMeRich.Domain.Entities;
 using MakeMeRich.Infrastructure.Persistance;
 
-using MediatR;
-
 using Microsoft.EntityFrameworkCore;
-using Moq;
 
 using Xunit;
 
@@ -20,18 +15,11 @@ namespace MakeMeRich.Application.IntegrationTests.FinancialAccounts.Commands
 {
     public class CreateFinancialAccountCommandHandlerTests
     {
-        private readonly CreateFinancialAccountCommandHandler _createFinancialAccountCommandHandler;
-        private readonly ApplicationDbContext _context;
-
         public CreateFinancialAccountCommandHandlerTests()
         {
             ContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: "createFinancialAccountCommandHandlerTest")
                 .Options ;
-
-            _context = new ApplicationDbContext(ContextOptions);
-            _createFinancialAccountCommandHandler =
-                 new CreateFinancialAccountCommandHandler(_context);
         }
 
         public DbContextOptions<ApplicationDbContext> ContextOptions { get; set; }
@@ -41,16 +29,22 @@ namespace MakeMeRich.Application.IntegrationTests.FinancialAccounts.Commands
         {
             var command = new CreateFinancialAccountCommand
             {
-                Title = "BNP Osobiste",
+                Title = "BNP Private",
                 CurrentBalance = 250
             };
 
-            int id = await _createFinancialAccountCommandHandler.Handle(command, new CancellationToken());
-            var financialAccount = await _context.FindAsync<FinancialAccount>(id);
+            using (var context = new ApplicationDbContext(ContextOptions))
+            {
+                var commandHandler =
+                    new CreateFinancialAccountCommandHandler(context);
 
-            financialAccount.Should().NotBeNull();
-            financialAccount.Title.Should().Be(command.Title);
-            financialAccount.CurrentBalance.Should().Be(250);
+                int id = await commandHandler.Handle(command, new CancellationToken());
+                var financialAccount = await context.FindAsync<FinancialAccount>(id);
+
+                financialAccount.Should().NotBeNull();
+                financialAccount.Title.Should().Be(command.Title);
+                financialAccount.CurrentBalance.Should().Be(250);
+            }
         }
     }
 }
---
+++ Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FluentValidation.Results;
+
+namespace MakeMeRich.Application.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            :base("One or more validation failures have occured.")
+        {
+
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            :this()
+        {
+            var failureGroups = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage);
+
+            foreach (var failureGroup in failureGroups)
+            {
+                var propertyName = failureGroup.Key;
+                var propertyFailures = failureGroup.ToArray();
+
+                Errors.Add(propertyName, propertyFailures);
+            }
+        }
+
+        public IDictionary<string, string[]> Errors { get;}
+    }
+}
