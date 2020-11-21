801c7702f9b83c7a3966a6a273a3921fd77349ab
Sebastian <sebastian.studniczek@outlook.com>
2020-11-21 21:17:44 +0100

    Create command for creating financial account

---
+++ Application/FinancialAccounts/Commands/CreateFinancialAccount/CreateFinancialAccountCommand.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+namespace MakeMeRich.Application.FinancialAccounts.Commands.CreateFinancialAccount
+{
+    public class CreateFinancialAccountCommand : IRequest<int>
+    {
+        public string Title { get; set; }
+        public double CurrentBalance { get; set; }
+    }
+
+    public class CreateFinancialAccountCommandHandler : IRequestHandler<CreateFinancialAccountCommand, int>
+    {
+        public Task<int> Handle(CreateFinancialAccountCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Application/FinancialAccounts/Commands/CreateFinancialAccount/CreateFinancialAccountCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeMeRich.Application.FinancialAccounts.Commands.CreateFinancialAccount
+{
+    public class CreateFinancialAccountCommandValidator
+    {
+
+    }
+}
