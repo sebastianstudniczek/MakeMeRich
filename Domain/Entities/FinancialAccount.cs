ce22a29c9330433c8723c74f7b6e4fa4336868bc
Sebastian <sebastian.studniczek@outlook.com>
2020-11-21 21:16:25 +0100

    Create financial account models

---
+++ Domain/Entities/FinancialAccount.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MakeMeRich.Domain.Entities
+{
+    public class FinancialAccount : BaseEntity
+    {
+        public FinancialAccount()
+        {
+            Transactions = new List<Transaction>();
+        }
+        public string Title { get; set; }
+        public double CurrentBalance { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+    }
+}
---
+++ Domain/Entities/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeMeRich.Domain.Entities
+{
+    public class Transaction : BaseEntity
+    {
+
+    }
+}
