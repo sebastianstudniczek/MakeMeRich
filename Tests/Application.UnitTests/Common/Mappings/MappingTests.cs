ce53b454a37a89934024b318ed66163bf1e45707
Sebastian <sebastian.studniczek@outlook.com>
2020-11-22 14:06:39 +0100

    Implement mapping test

---
+++ Tests/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using AutoMapper;
+
+using MakeMeRich.Application.Common.Mappings;
+using MakeMeRich.Application.FinancialAccounts.Queries;
+using MakeMeRich.Domain.Entities;
+
+using Xunit;
+
+namespace MakeMeRich.Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+                cfg.AddProfile<MappingProfile>());
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Theory]
+        [InlineData(typeof(FinancialAccount), typeof(FinancialAccountDto))]
+        [InlineData(typeof(FinancialTransaction), typeof(FinancialTransactionDto))]
+        public void ShouldMapFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = Activator.CreateInstance(source);
+            _mapper.Map(instance, source, destination);
+        }
+    }
+}
