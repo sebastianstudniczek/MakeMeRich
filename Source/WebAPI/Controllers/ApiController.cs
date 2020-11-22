27f335155281fa98141db24dd9621e2f37819be6
Sebastian <sebastian.studniczek@outlook.com>
2020-11-22 19:44:58 +0100

    Create a base api controller

---
+++ Source/WebAPI/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakeMeRich.WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v1/[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
