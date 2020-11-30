01658be331bc065ce66a725e29de4c1d0b0423ac
Sebastian <sebastian.studniczek@outlook.com>
2020-11-30 11:49:08 +0100

    Simplify the financial account dto

--- Source/Application/Common/Dtos/FinancialAccountDto.cs
+++ Source/Application/Common/Dtos/FinancialAccountDto.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-
-using MakeMeRich.Application.Common.Dtos.FinancialTransactions;
-using MakeMeRich.Application.Common.Mappings;
+﻿using MakeMeRich.Application.Common.Mappings;
 using MakeMeRich.Domain.Entities;
 
 namespace MakeMeRich.Application.Common.Dtos
@@ -12,11 +9,5 @@ namespace MakeMeRich.Application.Common.Dtos
         public string Title { get; set; }
         public double CurrentBalance { get; set; }
         public string AccountType { get; set; }
-        public IList<ExternalTransactionDto> ExternalTransactions { get; set; }
-            = new List<ExternalTransactionDto>();
-        public IList<InternalTransactionDto> ReceivedInternalTransactions { get; set; }
-            = new List<InternalTransactionDto>();
-        public IList<InternalTransactionDto> SendedInternalTransactions { get; set; }
-            = new List<InternalTransactionDto>();
     }
 }
