bdd4d0db70a73b89edf6b40cc01b64d67244cfe0
Sebastian <sebastian.studniczek@outlook.com>
2020-12-01 19:04:54 +0100

    Simplify the financial category dto

--- Source/Application/Common/Dtos/FinancialCategoryDto.cs
+++ Source/Application/Common/Dtos/FinancialCategoryDto.cs
@@ -1,18 +1,11 @@
-﻿using MakeMeRich.Application.Common.Dtos.FinancialTransactionCategories;
-using MakeMeRich.Application.Common.Mappings;
+﻿using MakeMeRich.Application.Common.Mappings;
 using MakeMeRich.Domain.Entities;
-
-using System.Collections.Generic;
 
 namespace MakeMeRich.Application.Common.Dtos
 {
     public class FinancialCategoryDto : IMapFrom<FinancialCategory>
     {
         public int Id { get; set; }
-        public int Name { get; set; }
-        public IList<ExternalTransactionCategoryDto> ExternalTransactionCategories { get; set; }
-            = new List<ExternalTransactionCategoryDto>();
-        public IList<InternalTransactionCategoryDto> InternalTransactionCategories { get; set; }
-            = new List<InternalTransactionCategoryDto>();
+        public string Name { get; set; }
     }
 }
