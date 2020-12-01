7c1547bf3be95398a4d187336657147fb7b8de54
Sebastian <sebastian.studniczek@outlook.com>
2020-12-01 19:38:48 +0100

    Create endpoint for getting financial categories

---
+++ Source/WebAPI/Controllers/FinancialCategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using MakeMeRich.Application;
+using MakeMeRich.Application.Common.Dtos;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeMeRich.WebAPI.Controllers
+{
+    public class FinancialCategoriesController : ApiController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<FinancialCategoryDto>> GetAll()
+        {
+            return await Mediator.Send(new GetFinancialCategoriesQuery());
+        }
+
+    }
+}
