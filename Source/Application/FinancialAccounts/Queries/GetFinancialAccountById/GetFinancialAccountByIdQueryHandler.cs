29e9644d0fae7fc646e09e967c2e379c5592dda0
Sebastian <sebastian.studniczek@outlook.com>
2020-12-01 13:10:00 +0100

    Create base classes for getting financial account

---
+++ Source/Application/FinancialAccounts/Queries/GetFinancialAccountById/GetFinancialAccountByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+
+using MakeMeRich.Application.Common.Dtos;
+using MakeMeRich.Application.Common.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakeMeRich.Application.FinancialAccounts.Queries.GetFinancialAccountById
+{
+    public class GetFinancialAccountByIdQueryHandler
+    {
+        private IApplicationDbContext _context;
+        private IMapper _mapper;
+
+        public GetFinancialAccountByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<FinancialAccountDto> Handle(GetFinancialAccountByIdQuery query, CancellationToken none)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/Application.UnitTests/FinancialAccounts/Queries/GetFinancialAccountByIdCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+
+using FluentAssertions;
+
+using MakeMeRich.Application.Common.Mappings;
+using MakeMeRich.Application.FinancialAccounts.Queries.GetFinancialAccountById;
+using MakeMeRich.Application.UnitTests.Common;
+using MakeMeRich.Application.UnitTests.Helper;
+using MakeMeRich.Infrastructure.Persistance;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace MakeMeRich.Application.UnitTests.FinancialAccounts.Queries
+{
+    public class GetFinancialAccountByIdCommandHandlerTests : HandlerTest
+    {
+        private readonly MapperConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public GetFinancialAccountByIdCommandHandlerTests()
+        {
+            _configuration = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFinancialAccount()
+        {
+            DataSeeder.GetSampleFinancialAccounts(DbContextOptions);
+            var query = new GetFinancialAccountByIdQuery { Id = 2 };
+
+            using (var context = new ApplicationDbContext(DbContextOptions))
+            {
+                var queryHandler = new GetFinancialAccountByIdQueryHandler(context, _mapper);
+                var result = await queryHandler.Handle(query, CancellationToken.None);
+
+                result.Should().NotBeNull();
+            }
+        }
+    }
+}
