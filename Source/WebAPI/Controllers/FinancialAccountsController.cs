6803418545f20454a6999e36a2e8c87662d13430
Sebastian <sebastian.studniczek@outlook.com>
2020-12-01 18:01:57 +0100

    Create endpoint to get financial account by id

--- Source/WebAPI/Controllers/FinancialAccountsController.cs
+++ Source/WebAPI/Controllers/FinancialAccountsController.cs
@@ -8,6 +8,7 @@ using MakeMeRich.Application.FinancialAccounts.Commands.CreateFinancialAccount;
 using MakeMeRich.Application.FinancialAccounts.Commands.DeleteFinancialAccount;
 using MakeMeRich.Application.FinancialAccounts.Commands.UpdateFinancialAccount;
 using MakeMeRich.Application.FinancialAccounts.Queries;
+using MakeMeRich.Application.FinancialAccounts.Queries.GetFinancialAccountById;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,14 @@ namespace MakeMeRich.WebAPI.Controllers
         public async Task<ActionResult<List<FinancialAccountDto>>> GetAll()
         {
             return await Mediator.Send(new GetFinancialAccountsQuery());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<FinancialAccountDto>> GetById(int id)
+        {
+            return await Mediator.Send(new GetFinancialAccountByIdQuery { Id = id });
         }
 
         [HttpPost]
