9ab67d86884787d27f2a453fd8f753711d9478d0
Sebastian <sebastian.studniczek@outlook.com>
2020-12-01 19:18:12 +0100

    Implement query to get all financial categories

---
+++ Source/Application/FinancialCategories/Queries/GetFinancialCategories/GetFinancialCategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MakeMeRich.Application.Common.Dtos;
+using MediatR;
+using System.Collections.Generic;
+
+namespace MakeMeRich.Application
+{
+    public class GetFinancialCategoriesQuery : IRequest<List<FinancialCategoryDto>>
+    {
+    }
+}
---
+++ Source/Application/FinancialCategories/Queries/GetFinancialCategories/GetFinancialCategoriesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+using MakeMeRich.Application.Common.Dtos;
+using MakeMeRich.Application.Common.Interfaces;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakeMeRich.Application
+{
+    public class GetFinancialCategoriesQueryHandler : IRequestHandler<GetFinancialCategoriesQuery, List<FinancialCategoryDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetFinancialCategoriesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<List<FinancialCategoryDto>> Handle(GetFinancialCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return _context.FinancialCategories
+                .ProjectTo<FinancialCategoryDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
