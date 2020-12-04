bc79068fc60bcb7eccc3a93cd0eb1b81f727a45b
Sebastian <sebastian.studniczek@outlook.com>
2020-12-04 15:45:50 +0100

    Refactor financial category commands and queries
    The handlers have been moved to query/command classes to simplify
    the code structure.

--- Source/Application/FinancialCategories/Queries/GetFinancialCategories/GetFinancialCategoriesQuery.cs
+++ Source/Application/FinancialCategories/Queries/GetFinancialCategories/GetFinancialCategoriesQuery.cs
@@ -1,10 +1,37 @@
-﻿using MakeMeRich.Application.Common.Dtos;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MakeMeRich.Application.Common.Dtos;
+using MakeMeRich.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MakeMeRich.Application
 {
     public class GetFinancialCategoriesQuery : IRequest<List<FinancialCategoryDto>>
     {
+
+    }
+
+    public class GetFinancialCategoriesQueryHandler : IRequestHandler<GetFinancialCategoriesQuery, List<FinancialCategoryDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetFinancialCategoriesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<FinancialCategoryDto>> Handle(GetFinancialCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.FinancialCategories
+                .ProjectTo<FinancialCategoryDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
+        }
     }
 }
--- Source/Application/FinancialCategories/Queries/GetFinancialCategoryById/GetFinancialCategoryByIdQuery.cs
+++ Source/Application/FinancialCategories/Queries/GetFinancialCategoryById/GetFinancialCategoryByIdQuery.cs
@@ -1,4 +1,10 @@
-﻿using MakeMeRich.Application.Common.Dtos;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MakeMeRich.Application.Common.Dtos;
+using MakeMeRich.Application.Common.Exceptions;
+using MakeMeRich.Application.Common.Interfaces;
+using MakeMeRich.Domain.Entities;
 using MediatR;
 
 namespace MakeMeRich.Application.FinancialCategories.Queries.GetFinancialCategoryById
@@ -6,5 +12,31 @@ namespace MakeMeRich.Application.FinancialCategories.Queries.GetFinancialCategor
     public class GetFinancialCategoryByIdQuery : IRequest<FinancialCategoryDto>
     {
         public int Id { get; set; }
+    }
+
+    public class GetFinancialCategoryByIdQueryHandler : IRequestHandler<GetFinancialCategoryByIdQuery, FinancialCategoryDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetFinancialCategoryByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<FinancialCategoryDto> Handle(GetFinancialCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.FinancialCategories
+                .FindAsync(new object[] { request.Id }, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(FinancialCategory), request.Id);
+            }
+
+            return _mapper.Map<FinancialCategoryDto>(entity);
+        }
     }
 }
