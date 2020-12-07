9fb9435306ff68527be66f6c9a91a97fb1b9e914
Sebastian <sebastian.studniczek@outlook.com>
2020-12-07 22:29:00 +0100

    Add financial transactions controller

---
+++ Source/WebAPI/Controllers/FinancialTrancationsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeMeRich.WebAPI.Controllers
+{
+    public class FinancialTrancationsController
+    {
+    }
+}
