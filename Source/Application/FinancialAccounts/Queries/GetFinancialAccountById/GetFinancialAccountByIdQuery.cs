558270bca045966885998ac165038445d8b52e94
Sebastian <sebastian.studniczek@outlook.com>
2020-12-10 21:53:07 +0100

    Refactor financial accounts queries

--- Source/Application/FinancialAccounts/Queries/GetFinancialAccountById/GetFinancialAccountByIdQuery.cs
+++ Source/Application/FinancialAccounts/Queries/GetFinancialAccountById/GetFinancialAccountByIdQuery.cs
@@ -1,4 +1,10 @@
-﻿using MakeMeRich.Application.Common.Dtos;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MakeMeRich.Application.Common.Dtos;
+using MakeMeRich.Application.Common.Exceptions;
+using MakeMeRich.Application.Common.Interfaces;
+using MakeMeRich.Domain.Entities;
 using MediatR;
 
 namespace MakeMeRich.Application.FinancialAccounts.Queries.GetFinancialAccountById
@@ -6,5 +12,30 @@ namespace MakeMeRich.Application.FinancialAccounts.Queries.GetFinancialAccountBy
     public class GetFinancialAccountByIdQuery : IRequest<FinancialAccountDto>
     {
         public int Id { get; set; }
+    }
+    public class GetFinancialAccountByIdQueryHandler : IRequestHandler<GetFinancialAccountByIdQuery, FinancialAccountDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetFinancialAccountByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<FinancialAccountDto> Handle(GetFinancialAccountByIdQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.FinancialAccounts
+                .FindAsync(new object[] { request.Id }, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(FinancialAccount), request.Id);
+            }
+
+            return _mapper.Map<FinancialAccountDto>(entity);
+        }
     }
 }
--- Source/Application/FinancialAccounts/Queries/GetFinancialAccounts/GetFinancialAccountsQuery.cs
+++ Source/Application/FinancialAccounts/Queries/GetFinancialAccounts/GetFinancialAccountsQuery.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MakeMeRich.Application.Common.Dtos;
+using MakeMeRich.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace MakeMeRich.Application.FinancialAccounts.Queries.GetFinancialAccounts
 {
     public class GetFinancialAccountsQuery : IRequest<List<FinancialAccountDto>>
     {
 
+    }
+    public class GetFinancialAccountsQueryHandler : IRequestHandler<GetFinancialAccountsQuery, List<FinancialAccountDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetFinancialAccountsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<List<FinancialAccountDto>> Handle(GetFinancialAccountsQuery request, CancellationToken cancellationToken)
+        {
+            return _context.FinancialAccounts
+                    .ProjectTo<FinancialAccountDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
+        }
     }
 }
